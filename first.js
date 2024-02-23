233126d7c22b02b35a7e8ccae69b3ea7ac09bff8
AgneeshG <g.r.agneesh2112@gmail.com>
2024-02-23 12:47:49 -0800

    created new first.js,firstjs.html file

---
+++ first.js
@@ -0,0 +1,26 @@
+// variable
+var name = "Anbu";
+let name1 = "Banu";
+const name2 = "Vetri";
+alert("His name is " + name);
+console.log(name1 + " is 32 years old.");
+
+// objects 
+let person = {
+    name: "Jack",
+    age: 21,
+    gender: "male",
+    address:{
+        area: "Anna Nagar",
+        town: "Eachanari",
+        dist: "Coimbatore"
+    }
+};
+console.log(person);
+console.log(person.age);
+console.log(person.address.town);
+
+// array
+let favColors = ["White","Green","Blue","Orange","Purple"];
+console.log(favColors);
+console.log(favColors[2]);
--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-console.log("Sample js program")
-var name1 = "Arun"
-let name2 = "Abi"
-const dob = "21-01-2020"
-console.log(name1)
-console.log("Name of the person : "+name1+ " and his data of birth " + dob)
-console.log("One")
-console.log("Two")
+console.log("Sample js program");
+var name1 = "Arun";
+let name2 = "Abi";
+const dob = "21-01-2020";
+console.log("Name of the person : "+name1+ " and his data of birth " + dob);
+
+// objects
+let person = {
+    name: "Vel",
+    age : 31,
+    gender: "male",
+    city: "Pollachi"
+}
+let person2 = {}
+let person3 = {
+    name:"Ram",
+    age:27,
+    gender:"male",
+    siblings:{
+        brother:"Varun",
+        sister:"Devi"
+    },
+    city:"coimbatore",
+}
+
+// array
+let favColors = ["red","blue","white","orange"]
+console.log(person2)
+console.log(person)
+console.log(person.age)
+console.log(person['gender'])
+console.log(person3.siblings)
+console.log(person3.siblings.brother)
+console.log(favColors);
