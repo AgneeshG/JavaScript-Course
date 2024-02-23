92703e32c20f94b5a221b521ffd640860e8a2284
AgneeshG <g.r.agneesh2112@gmail.com>
2024-02-23 10:03:05 -0800

    added few line of code in index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log("Sample js program")
+var name1 = "Arun"
+let name2 = "Abi"
+const dob = "21-01-2020"
+console.log(name1)
+console.log("Name of the person : "+name1+ " and his data of birth " + dob)
